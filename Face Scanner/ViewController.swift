d65b89af2636aac0612736eb520700351b677ce5
sandeepspyne <sandeep.singh@spyne.ai>
2024-02-05 00:55:11 +0530

    Initial Commit

---
+++ Face Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Face Scanner
+//
+//  Created by sandeepsing-maclaptop on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
